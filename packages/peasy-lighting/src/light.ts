b1f7efe50b46304a10a76fd17ad1927a90aa9074
Jürgen Wenzel <jurgen@jwenzel.se>
2023-02-02 16:35:06 +0100

    feat(lighting): add external entity to light

--- packages/peasy-lighting/src/light.ts
+++ packages/peasy-lighting/src/light.ts
@@ -1,14 +1,22 @@
 import { Entity } from "./entity";
 import { LightEntity } from "./light-entity";
 import { Lighting } from "./lighting";
-import { Vector } from "./vector";
+import { IVector, Vector } from "./vector";
 import { Viewport } from "./viewport";
 
 export interface ILight extends Partial<Omit<Light, 'viewport'>> {
   viewport: Viewport | HTMLElement;
 }
 
+export interface IExternalLight {
+  position: IVector;
+  radius: number;
+  zIndex?: number;
+  color?: string;
+}
+
 export class Light {
+  public entity!: IExternalLight;
   public id!: string;
 
   public containerElement!: HTMLElement;
@@ -18,125 +26,127 @@ export class Light {
     position: 0,
     zIndex: 0,
   };
-  #updated: Set<string> = new Set();
+  private readonly _updated: Set<string> = new Set();
 
-  #position: Vector = new Vector();
-  #radius = 0;
-  #zIndex = 0;
-  #size = 0;
-  #displayDistance = 0;
-  #display!: 'inline-block' | 'none';
-  #color!: string;
-  #gradientSteps: string | (number | string)[] = [];
-  #gradient!: string;
+  private _position: Vector = new Vector();
+  private _radius = 0;
+  private _zIndex = 0;
+  private _size = 0;
+  private _displayDistance = 0;
+  private _display!: 'inline-block' | 'none';
+  private _color!: string;
+  private _gradientSteps: string | (number | string)[] = [];
+  private _gradient!: string;
 
-  #viewport!: Viewport;
-  #colorElement!: HTMLElement;
-  #lightElement!: HTMLElement;
+  private _viewport!: Viewport;
+  private _colorElement!: HTMLElement;
+  private _lightElement!: HTMLElement;
 
-  #entities: WeakMap<Entity, LightEntity> = new WeakMap();
+  private readonly _entities: WeakMap<Entity, LightEntity> = new WeakMap();
 
   public get position(): Vector {
-    return this.#position;
+    return this._position;
   }
-  public set position(value: Vector) {
-    if (value.x === this.#position.x && value.y === this.#position.y) {
-      this.#position = value;
+  public set position(value: IVector) {
+    if (value.x === this._position.x && value.y === this._position.y) {
+      if (value instanceof Vector) {
+        this._position = value;
+      }
       return;
     }
-    this.#position = value;
-    this.#updated.add('position');
+    this._position = value instanceof Vector ? value : new Vector(value.x, value.y);
+    this._updated.add('position');
     this.updates.position++;
     this.version++;
   }
 
   public get x(): number {
-    return this.#position.x;
+    return this._position.x;
   }
   public set x(value: number) {
-    if (this.#position.x === value) {
+    if (this._position.x === value) {
       return;
     }
-    this.#position.x = value;
-    this.#updated.add('position');
+    this._position.x = value;
+    this._updated.add('position');
     this.updates.position++;
     this.version++;
   }
   public get y(): number {
-    return this.#position.y;
+    return this._position.y;
   }
   public set y(value: number) {
-    if (this.#position.y === value) {
+    if (this._position.y === value) {
       return;
     }
-    this.#position.y = value;
-    this.#updated.add('position');
+    this._position.y = value;
+    this._updated.add('position');
     this.updates.position++;
     this.version++;
   }
 
   public get radius(): number {
-    return this.#radius;
+    return this._radius;
   }
   public set radius(value: number) {
-    if (value === this.#radius) {
+    if (value === this._radius) {
       return;
     }
-    this.#radius = value;
-    const max = Math.max(this.#viewport.size.x, this.#viewport.size.y);
-    const distance = (max / 2) + this.#radius;
-    if (distance > this.#displayDistance) {
-      this.#displayDistance = distance;
-      this.#size = (max + this.#radius) * 2;
+    this._radius = value;
+    const max = Math.max(this._viewport.size.x, this._viewport.size.y);
+    const distance = (max / 2) + this._radius;
+    if (distance > this._displayDistance) {
+      this._displayDistance = distance;
+      this._size = (max + this._radius) * 2;
 
-      this.#updated.add('size');
-      this.#updated.add('position');
+      this._updated.add('size');
+      this._updated.add('position');
       // this.updates.size++;
       this.updates.position++;
       this.version++;
     }
-    if (Array.isArray(this.#gradientSteps)) {
-      this.#updateGradient();
-      this.#updated.add('gradient');
+    if (Array.isArray(this._gradientSteps)) {
+      this._updateGradient();
+      this._updated.add('gradient');
     }
   }
 
   public get zIndex(): number {
-    return this.#zIndex;
+    return this._zIndex;
   }
   public set zIndex(value: number) {
-    if (this.#zIndex === value) {
+    if (this._zIndex === value) {
       return;
     }
-    this.#zIndex = value;
+    this._zIndex = value;
     this.updates.zIndex++;
     this.version++;
   }
 
   public get color(): string {
-    return this.#color;
+    return this._color;
   }
   public set color(value: string) {
-    if (value === this.#color) {
+    if (value === this._color) {
       return;
     }
-    this.#color = value;
-    this.#updated.add('color');
+    this._color = value;
+    this._updated.add('color');
   }
 
   public get gradientSteps(): string | (number | string)[] {
-    return this.#gradientSteps;
+    return this._gradientSteps;
   }
   public set gradientSteps(value: string | (number | string)[]) {
     if (typeof value === 'string') {
-      this.#gradientSteps = value;
-      this.#updated.add('gradient');
+      this._gradientSteps = value;
+      this._updated.add('gradient');
       return;
     }
-    let different = value.length !== this.#gradientSteps.length;
+    let different = value.length !== this._gradientSteps.length;
     if (!different) {
       for (let i = 0; i < value.length; i++) {
-        if (value[i] !== this.#gradientSteps[i]) {
+        if (value[i] !== this._gradientSteps[i]) {
           different = true;
           break;
         }
@@ -145,17 +155,17 @@ export class Light {
     if (!different) {
       return;
     }
-    this.#gradientSteps = value;
-    this.#gradient = this.#gradientSteps.join(',');
-    this.#updated.add('gradient');
+    this._gradientSteps = value;
+    this._gradient = this._gradientSteps.join(',');
+    this._updated.add('gradient');
     return;
   }
 
   private get left(): number {
-    return this.#position.x - (this.#size / 2);
+    return this._position.x - (this._size / 2);
   }
   private get top(): number {
-    return this.#position.y - (this.#size / 2);
+    return this._position.y - (this._size / 2);
   }
 
   private constructor() {
@@ -163,89 +173,110 @@ export class Light {
     this.gradientSteps = ['white', 0.25, 'black', 1];
   }
 
-  public static create(input: ILight): Light {
+  public static create(input: ILight | IExternalLight): Light {
     const light = new Light();
 
-    light.id = input.id as string;
-    light.#viewport = Lighting.addViewport(input.viewport);
-    light.#position = input.position ?? light.#position;
-    light.radius = input.radius ?? light.radius;
-    light.zIndex = input.zIndex ?? light.#zIndex;
-    light.color = input.color ?? light.color;
+    light.entity = (input as ILight).entity ?? input as IExternalLight;
+
+    light.id = (input as ILight).id as string;
+    light._viewport = Lighting.addViewport((input as ILight).viewport);
+    light.position = new Vector(light.entity.position.x, light.entity.position.y);
+    light.radius = light.entity.radius ?? light.radius;
+    light.zIndex = light.entity.zIndex ?? light.zIndex;
+    light.color = light.entity.color ?? light.color;
+
+    // light._position = input.position ?? light._position;
+    // light.radius = input.radius ?? light.radius;
+    // light.zIndex = input.zIndex ?? light._zIndex;
+    // light.color = input.color ?? light.color;
 
     return light;
   }
 
   public update(): void {
-    if (this.#viewport == null) {
+    if (this._viewport == null) {
       return;
     }
-    if (this.#lightElement == null) {
-      this.#createElements();
+    if (this._lightElement == null) {
+      this._createElements();
 
-      this.#updated.add('size');
-      this.#updated.add('position');
-      this.#updated.add('gradient');
-      this.#updated.add('color');
+      this._updated.add('size');
+      this._updated.add('position');
+      this._updated.add('gradient');
+      this._updated.add('color');
       // this.updates.size++;
       this.updates.position++;
       this.version++;
     }
-    this.#updateProperties();
-    this.#updated.clear();
+    this._updateFromEntity();
+    this._updateProperties();
+    this._updated.clear();
 
     Lighting.entities.forEach((entity) => {
-      if (!this.#entities.has(entity)) {
-        this.#entities.set(entity, LightEntity.create({ light: this, entity }));
+      if (!this._entities.has(entity)) {
+        this._entities.set(entity, LightEntity.create({ light: this, entity }));
       }
-      this.#entities.get(entity)!.update();
+      this._entities.get(entity)!.update();
     });
   }
 
-  #updateProperties(): void {
-    for (const update of this.#updated) {
+  private _updateFromEntity(): void {
+    this.position = this.entity.position;
+    if (this.entity.radius != null) {
+      this.radius = this.entity.radius;
+    }
+    if (this.entity.zIndex != null) {
+      this.zIndex = this.entity.zIndex;
+    }
+    if (this.entity.color != null) {
+      this.color = this.entity.color;
+    }
+  }
+
+  private _updateProperties(): void {
+    for (const update of this._updated) {
       // console.log('Updating light:', update);
       switch (update) {
         case 'size':
-          this.#lightElement.style.width = `${this.#size}px`;
-          this.#lightElement.style.height = `${this.#size}px`;
+          this._lightElement.style.width = `${this._size}px`;
+          this._lightElement.style.height = `${this._size}px`;
           break;
         case 'position': {
-          // console.log('Update light', this.#color, this.position);
-          const distance = this.position.subtract(this.#viewport.position);
-          const outside = Math.abs(distance.x) > this.#displayDistance || Math.abs(distance.y) > this.#displayDistance;
+          // console.log('Update light', this._color, this.position);
+          const distance = this.position.subtract(this._viewport.position);
+          const outside = Math.abs(distance.x) > this._displayDistance || Math.abs(distance.y) > this._displayDistance;
           const display = outside ? 'none' : 'inline-block';
-          // console.log('display', outside, display !== this.#display, this.position, this.#viewport.position);
-          if (display !== this.#display) {
-            this.#display = display;
-            this.#colorElement.style.display = this.#display;
+          // console.log('display', outside, display !== this._display, this.position, this._viewport.position);
+          if (display !== this._display) {
+            this._display = display;
+            this._colorElement.style.display = this._display;
           }
           if (display !== 'none') {
-            this.#lightElement.style.translate = `${this.left}px ${this.top}px`;
+            this._lightElement.style.translate = `${this.left}px ${this.top}px`;
           }
           break;
         }
         case 'color':
-          this.#colorElement.style.background = this.#color;
+          this._colorElement.style.background = this._color;
           break;
         case 'gradient':
-          this.#lightElement.style.background = `radial-gradient(${this.#gradient})`;
+          this._lightElement.style.background = `radial-gradient(${this._gradient})`;
           break;
       }
     }
   }
 
-  #updateGradient(): void {
+  private _updateGradient(): void {
     const steps = [];
-    for (let i = 0, ii = this.#gradientSteps.length; i < ii; i += 2) {
-      const [color, step] = this.#gradientSteps.slice(i, i + 2);
-      steps.push(`${color} ${typeof step === 'number' ? `${this.#radius * step}px` : step}`);
+    for (let i = 0, ii = this._gradientSteps.length; i < ii; i += 2) {
+      const [color, step] = this._gradientSteps.slice(i, i + 2);
+      steps.push(`${color} ${typeof step === 'number' ? `${this._radius * step}px` : step}`);
     }
-    this.#gradient = steps.join(',');
+    this._gradient = steps.join(',');
   }
 
-  #createElements() {
-    this.#viewport.vail.insertAdjacentHTML('beforeend', `
+  private _createElements() {
+    this._viewport.vail.insertAdjacentHTML('beforeend', `
       <div class="color" style="
         display: inline-block;
         position: absolute;
@@ -273,8 +304,8 @@ export class Light {
           "></div>
         </div>
       </div>`);
-    this.#colorElement = this.#viewport.vail.lastElementChild as HTMLElement;
-    this.containerElement = this.#colorElement.firstElementChild as HTMLElement;
-    this.#lightElement = this.containerElement.firstElementChild as HTMLElement;
+    this._colorElement = this._viewport.vail.lastElementChild as HTMLElement;
+    this.containerElement = this._colorElement.firstElementChild as HTMLElement;
+    this._lightElement = this.containerElement.firstElementChild as HTMLElement;
   }
 }
